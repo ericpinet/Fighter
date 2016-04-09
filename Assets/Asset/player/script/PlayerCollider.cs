7652bc09f4b4f32e43433dd93f6eb2abb970d3f4
Eric Pinet <pineri01@gmail.com>
2016-04-09 13:56:11 -0400

    Create project base

---
+++ Assets/Asset/player/script/PlayerCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Detect Collision
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("IsHitable")) {
+			Debug.Log ("Hit!");
+			other.gameObject.GetComponentInParent<Rigidbody> ().AddForce(Vector3.up * 1000);
+		}
+	}
+}
---
+++ Assets/Asset/player/script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Player animator
+	private Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get the animator controller of the player
+		m_animator = GetComponent<Animator> ();
+		//m_animator.SetTrigger ("WalkBegin");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// check if the user ask for walk
+		if (Input.GetAxis ("Horizontal") > 0) { 
+			m_animator.SetBool("IsWalking", true);
+		} else {
+			m_animator.SetBool("IsWalking", false);
+		}
+
+		// check if the user ask for back
+		if (Input.GetAxis ("Horizontal") < 0) { 
+			m_animator.SetBool("IsBacking", true);
+		} else {
+			m_animator.SetBool("IsBacking", false);
+		}
+
+		// Punch!
+		if (Input.GetButtonDown ("Fire1")) {
+			m_animator.SetTrigger("Punch");
+		}
+
+	}
+}
